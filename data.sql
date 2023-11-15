f7cb95eb94a63c30eae099feda2330d4111303ca
antsa <antsafiderana333@gmail.com>
2023-11-15 11:38:00 +0300

    Add Rate and Review entity

--- data.sql
+++ data.sql
@@ -1,18 +1,46 @@
 -- Insert data into the 'club' table
-INSERT INTO club (id, name) VALUES
-                                (1, 'Book Club 1'),
-                                (2, 'Book Club 2');
+INSERT INTO club (id, name)
+VALUES (1, 'Book Club 1'),
+       (2, 'Book Club 2');
 
 -- Insert data into the 'author' table
-INSERT INTO author (id, club_id, first_name, last_name) VALUES
-                                                            (1, 1, 'John', 'Doe'),
-                                                            (2, 1, 'Jane', 'Smith'),
-                                                            (3, 2, 'Bob', 'Johnson');
+INSERT INTO author (id, club_id, first_name, last_name)
+VALUES (1, 1, 'John', 'Doe'),
+       (2, 1, 'Jane', 'Smith'),
+       (3, 2, 'Bob', 'Johnson');
 
 -- Insert data into the 'book' table
-INSERT INTO book (id, author_id, title, description) VALUES
-                                                         (1, 1, 'The Art of Fiction', 'A guide to writing fiction'),
-                                                         (2, 2, 'Data Science 101', 'An introduction to data science'),
-                                                         (3, 3, 'History of the Universe', 'From the Big Bang to present');
+INSERT INTO book (id, author_id, title, description)
+VALUES (1, 1, 'The Art of Fiction', 'A guide to writing fiction'),
+       (2, 2, 'Data Science 101', 'An introduction to data science'),
+       (3, 3, 'History of the Universe', 'From the Big Bang to present');
 
--- You can continue inserting more data as needed.
+-- Insert more test data into the review table
+INSERT INTO review (id, book_id, content)
+VALUES (4, 2, 'Enjoyed the characters.'),
+       (5, 3, 'Interesting plot twists.'),
+       (6, 1, 'Could not put it down.'),
+       (7, 3, 'Disappointed with the ending.'),
+       (8, 1, 'Highly recommend for mystery lovers.');
+
+-- Insert more test data into the rate table
+INSERT INTO rate (id, book_id, score)
+VALUES (4, 2, 5),
+       (5, 3, 4),
+       (6, 1, 5),
+       (7, 3, 2),
+       (8, 1, 4);
+
+ALTER SEQUENCE review_id_seq RESTART WITH 100;
+
+-- Alter the starting value for book_id_seq
+ALTER SEQUENCE book_id_seq RESTART WITH 100;
+
+-- Alter the starting value for club_id_seq
+ALTER SEQUENCE club_id_seq RESTART WITH 100;
+
+-- Alter the starting value for author_id_seq
+ALTER SEQUENCE author_id_seq RESTART WITH 100;
+
+-- Alter the starting value for rate_id_seq
+ALTER SEQUENCE rate_id_seq RESTART WITH 100;
---
+++ src/Entity/Review.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ReviewRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ReviewRepository::class)]
+class Review
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $content = null;
+
+    #[ORM\ManyToOne]
+    private ?Book $book = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): static
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getBook(): ?Book
+    {
+        return $this->book;
+    }
+
+    public function setBook(?Book $book): static
+    {
+        $this->book = $book;
+
+        return $this;
+    }
+}
