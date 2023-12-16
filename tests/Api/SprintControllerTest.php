a1f370e8a4284f903bff3a0ef4fb165246556241
antsa <antsafiderana333@gmail.com>
2023-12-16 11:38:56 +0300

    Test : Find all sprint with status on going

--- tests/Api/SprintControllerTest.php
+++ tests/Api/SprintControllerTest.php
@@ -30,6 +30,12 @@ class SprintControllerTest extends ApiTestCase
         self::assertJsonContains($expected);
     }
 
+    public function test_FindAllSprintByStatus_GivenStatusIsOnGoing() {
+        $response = self::createClient()->request('GET','projects/1/sprints?status=ongoing');
+        $expected = [2, 3, 6];
+        self::assertSame($expected, $this->exctractPropertyFromResponse($response->toArray(),'id'));
+    }
+
     public function test_CreateNewSprint() {
         self::createClient()->request('POST', 'projects/1/sprints', [
             'json' => [
@@ -53,6 +59,12 @@ class SprintControllerTest extends ApiTestCase
         ]);
         self::assertResponseIsSuccessful();
         self::assertJsonContains($updatedSprint);
+    }
+
+    private function exctractPropertyFromResponse($response, $property) {
+        return array_map(function ($sprint) use ($property) {
+            return $sprint['sprint'][$property];
+        }, $response);
     }
 
 }
